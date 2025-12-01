fb790f65309738c09fc509651d4455eb03506913
IsraMejia <ihmaisrael@gmail.com>
2025-11-30 18:46:03 -0600

    Codigo curso, voy a modificarlo auno mas moderno que no cause erroes

--- djangoProjects/ecommerce/cart/cart.py
+++ djangoProjects/ecommerce/cart/cart.py
@@ -9,5 +9,15 @@ class Cart():
         if 'session_key' not in request.session :
             cart = self.session['session'] = {} #New user - Create a new session for the user
 
-        self.cart = cart #save the session of the car to hava access in any part of our page   
+        self.cart = cart #save the session of the car to have access in any part of our page   
+    
+    def add(self, product, product_quantity):
+        product_id = str(product.id)
+        
+        if product_id in self.cart:
+            self.cart[product_id]['product_quantity'] = product_quantity
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_quantity}
+            
+        self.session.modified = True 
         
