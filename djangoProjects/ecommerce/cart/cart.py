00c187e35cc71a0f51f68a5c4ce3dc1c641f1492
IsraMejia <ihmaisrael@gmail.com>
2025-12-01 21:45:51 -0600

    Add feature cart ready

--- djangoProjects/ecommerce/cart/cart.py
+++ djangoProjects/ecommerce/cart/cart.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from store.models import Product
 
 class Cart():
 
@@ -7,20 +9,42 @@ class Cart():
         cart = self.session.get('session_key')
        
         if 'session_key' not in request.session :
-            cart = self.session['session'] = {} #New user - Create a new session for the user
+            cart = self.session['session_key'] = {} #New user - Create a new session for the user
 
         self.cart = cart #save the session of the car to have access in any part of our page   
     
-    def add(self, product, product_quantity):
+    def add(self, product, product_qty):
         product_id = str(product.id)
         
         # Lógica para agregar o actualizar
         if product_id in self.cart:
-            self.cart[product_id]['qty'] = product_quantity 
+            self.cart[product_id]['qty'] = product_qty 
         else:
-            # CORRECCIÓN: Asegúrate de guardar 'qty' consistentemente
-            self.cart[product_id] = {'price': str(product.price), 'qty': product_quantity}
+            self.cart[product_id] = {'price': str(product.price), 'qty': product_qty}
         
         # Marcar la sesión como modificada para que Django la guarde
         self.session.modified = True
+         
+    
+    def __len__(self):
+        return sum( item['qty'] for item in self.cart.values() )
+     
+    
+    def __iter__(self):
+        all_products_ids = self.cart.keys()
+        products = Product.objects.filter(id__in = all_products_ids)
+        cart = self.cart.copy()
+        
+        for product in products:
+            cart[str(product.id)] ['product'] = product 
+            
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total'] = item['price'] * item['qty'] #total price in this item cart
+            yield item            
+            
+    
+    def get_total(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+        
         
--- djangoProjects/ecommerce/cart/views.py
+++ djangoProjects/ecommerce/cart/views.py
@@ -6,21 +6,23 @@ from django.http import JsonResponse
 
 
 def cart_summary(request):
-    return render(request, 'cart/cart-summary.html')
+    cart = Cart(request)
+    return render(request, 'cart/cart-summary.html', {'cart': cart})
 
 def cart_add(request):
-    cart = Cart(request) 
-    
+    cart = Cart(request)     
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
         
         product = get_object_or_404(Product, id=product_id)
         
-        cart.add(product=product, product_quantity=product_quantity)
+        cart.add(product=product, product_qty=product_quantity)
         
-        # CORRECCIÓN: Simplifiqué las llaves del JSON para que sea más fácil leer en consola
-        response = JsonResponse({'product': product.title, 'qty': product_quantity})
+        cart_quantity = cart.__len__()
+        
+        response = JsonResponse({'qty': cart_quantity})
+        
         return response
 
 
