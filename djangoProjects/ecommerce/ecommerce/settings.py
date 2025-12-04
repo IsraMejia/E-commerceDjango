3e453aa1fe1c3b8811bac72f74a97d047148d36e
IsraMejia <ihmaisrael@gmail.com>
2025-12-03 22:30:23 -0600

    Version antes de reajustar sistema de archivos

--- djangoProjects/ecommerce/ecommerce/settings.py
+++ djangoProjects/ecommerce/ecommerce/settings.py
@@ -137,3 +137,6 @@ MEDIA_ROOT = BASE_DIR / 'static/media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORRECCIÓN: usa 'mail' y escribe bien 'Backend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
