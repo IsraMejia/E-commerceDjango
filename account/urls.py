1ef95c2f98091b1a6e6d6dee601ad69214e3fbd0
IsraMejia <ihmaisrael@gmail.com>
2025-12-04 23:37:52 -0600

    Profile screens first version

--- account/urls.py
+++ account/urls.py
@@ -12,9 +12,13 @@ urlpatterns = [
     path('email-verification-failed', views.email_verification_failed, name = 'email-verification-failed'),
       
     #Loging and logout
-    path('my-login', views.my_login , name = 'my-login'),  
+    path('my-login', views.my_login , name = 'my-login'),    
+    path('user-logout', views.user_logout, name = 'user-logout'), 
     
-    
+    #Dashboard /Profile URLs
     path('dashboard', views.dashboard , name = 'dashboard'),  
+    path('profile-management', views.profile_management , name = 'profile-management'),  
+    path('delete-account', views.delete_account , name = 'delete-account'),   
+    
     
 ]
--- account/views.py
+++ account/views.py
@@ -9,6 +9,7 @@ from django.utils.http import urlsafe_base64_decode , urlsafe_base64_encode
 from django.contrib.auth.models import User
 from django.contrib.auth.models import auth 
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -93,8 +94,25 @@ def my_login(request):
         
     context  = {'form': form}
     return render(request, 'account/my-login.html', context=context)
+
+
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect('store')
+
+
             
-            
+@login_required(login_url='my-login')          
 def dashboard(request):
     return render(request, 'account/dashboard.html' )
 
+
+@login_required(login_url='my-login')  
+def profile_management(request):
+    return render(request, 'account/profile-management.html')
+
+
+@login_required(login_url='my-login')  
+def delete_account(request):
+    return render(request, 'account/delete-account.html')
